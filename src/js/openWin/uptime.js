26ed69a12753cdaa35c07a52cb0b246f81c6d230
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2021-02-07 23:29:33 -0500

    prepare cleanup

--- src/js/openWin/uptime.js
+++
@@ -1,7 +0,0 @@
-function openUptimeWin() {
-    const remote = require('electron').remote;
-    const BrowserWindow = remote.BrowserWindow;
-
-    var win = new BrowserWindow({ width: 800, height: 600, frame: false, maximize: false,  minimize: false, autoHideMenuBar: true, webPreferences: {webviewTag: true}});
-    win.loadURL(`file://${__dirname}/page/uptime.html`);
-}
