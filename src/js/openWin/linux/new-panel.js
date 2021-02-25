b35c2dd5e9da265c99e0007461a2bec5f224502a
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2021-02-24 22:54:48 -0500

    Beta 4

--- src/js/openWin/linux/new-panel.js
+++
@@ -1,7 +0,0 @@
-function openNewPanelL() {
-    const remote = require('electron').remote;
-    const BrowserWindow = remote.BrowserWindow;
-
-    var win = new BrowserWindow({ width: 800, height: 600, frame: false, autoHideMenuBar: true, webPreferences: {webviewTag: true}});
-    win.loadURL(`file://${__dirname}/page/panel.html`);
-}
