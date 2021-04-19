ab7ac86c5011de1c6210015ab83cf1d64bef86f2
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2021-04-18 22:51:30 -0400

    Fix News tab and script adjustments

---
+++ src/js/etc/news.js
@@ -0,0 +1,14 @@
+function openPost(pageName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("content_Tab_post_area");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink_news");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+document.getElementById("defaultOpenNews").click();
