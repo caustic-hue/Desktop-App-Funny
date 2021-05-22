87d0159e225bb5e3ec0d779895132f9c8da6faa8
Korbs <KorbsStudio@OnMail.com>
2021-05-22 13:45:48 -0400

    Polish and ready for Pre-Release 6

---
+++ src/js/newTerm.js
@@ -0,0 +1,4 @@
+function newTermWin() {
+    console.log('A new terminal window has been opened.');
+    window.open('../../new-window/terminal.html', '_blank', 'width=500,height=300frame=true,nodeIntegration=true,contextIsolation=true');
+}
